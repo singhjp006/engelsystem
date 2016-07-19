0efc4ddd76ea8191dcd9e38251a41455414d169d
kamishetty sreeja <sreeja25kamishetty@gmail.com>
2016-07-19 07:35:27 +0530

    Created model file for admin_active

--- includes/controller/admin_active_controller.php
+++ includes/controller/admin_active_controller.php
@@ -9,7 +9,7 @@ function admin_active() {
 
   $msg = "";
   $search = "";
-  $forced_count = sql_num_query("SELECT * FROM `User` WHERE `force_active`=1");
+  $forced_count = User_force_active_count();
   $count = $forced_count;
   $limit = "";
   $set_active = "";
@@ -36,21 +36,14 @@ function admin_active() {
     if ($ok)
       $limit = " LIMIT " . $count;
     if (isset($_REQUEST['ack'])) {
-      sql_query("UPDATE `User` SET `Aktiv` = 0 WHERE `Tshirt` = 0");
-      $users = sql_select("
-          SELECT `User`.*, COUNT(`ShiftEntry`.`id`) as `shift_count`, ${shift_sum_formula} as `shift_length`
-          FROM `User`
-          LEFT JOIN `ShiftEntry` ON `User`.`UID` = `ShiftEntry`.`UID`
-          LEFT JOIN `Shifts` ON `ShiftEntry`.`SID` = `Shifts`.`SID`
-          WHERE `User`.`Gekommen` = 1 AND `User`.`force_active`=0
-          GROUP BY `User`.`UID`
-          ORDER BY `force_active` DESC, `shift_length` DESC" . $limit);
+      User_update_activ_tshirt();
+      $users = User_select_set_active($shift_sum_formula, $limit); 
       $user_nicks = array();
       foreach ($users as $usr) {
-        sql_query("UPDATE `User` SET `Aktiv` = 1 WHERE `UID`='" . sql_escape($usr['UID']) . "'");
+        User_set_active($usr['UID']);
         $user_nicks[] = User_Nick_render($usr);
       }
-      sql_query("UPDATE `User` SET `Aktiv`=1 WHERE `force_active`=TRUE");
+      User_actice_force_active();
       engelsystem_log("These angels are active now: " . join(", ", $user_nicks));
 
       $limit = "";
@@ -64,7 +57,7 @@ function admin_active() {
     $id = $_REQUEST['active'];
     $user_source = User($id);
     if ($user_source != null) {
-      sql_query("UPDATE `User` SET `Aktiv`=1 WHERE `UID`='" . sql_escape($id) . "' LIMIT 1");
+      User_update_active($id);
       engelsystem_log("User " . User_Nick_render($user_source) . " is active now.");
       $msg = success(_("Angel has been marked as active."), true);
     } else
@@ -73,7 +66,7 @@ function admin_active() {
     $id = $_REQUEST['not_active'];
     $user_source = User($id);
     if ($user_source != null) {
-      sql_query("UPDATE `User` SET `Aktiv`=0 WHERE `UID`='" . sql_escape($id) . "' LIMIT 1");
+      User_update_inactive($id);
       engelsystem_log("User " . User_Nick_render($user_source) . " is NOT active now.");
       $msg = success(_("Angel has been marked as not active."), true);
     } else
@@ -82,7 +75,7 @@ function admin_active() {
     $id = $_REQUEST['tshirt'];
     $user_source = User($id);
     if ($user_source != null) {
-      sql_query("UPDATE `User` SET `Tshirt`=1 WHERE `UID`='" . sql_escape($id) . "' LIMIT 1");
+      User_update_tshirt($id);
       engelsystem_log("User " . User_Nick_render($user_source) . " has tshirt now.");
       $msg = success(_("Angel has got a t-shirt."), true);
     } else
@@ -91,21 +84,14 @@ function admin_active() {
     $id = $_REQUEST['not_tshirt'];
     $user_source = User($id);
     if ($user_source != null) {
-      sql_query("UPDATE `User` SET `Tshirt`=0 WHERE `UID`='" . sql_escape($id) . "' LIMIT 1");
+      User_update_not_tshirt($id);
       engelsystem_log("User " . User_Nick_render($user_source) . " has NO tshirt.");
       $msg = success(_("Angel has got no t-shirt."), true);
     } else
       $msg = error(_("Angel not found."), true);
   }
 
-  $users = sql_select("
-      SELECT `User`.*, COUNT(`ShiftEntry`.`id`) as `shift_count`, ${shift_sum_formula} as `shift_length`
-      FROM `User` LEFT JOIN `ShiftEntry` ON `User`.`UID` = `ShiftEntry`.`UID`
-      LEFT JOIN `Shifts` ON `ShiftEntry`.`SID` = `Shifts`.`SID`
-      WHERE `User`.`Gekommen` = 1
-      " . ($show_all_shifts ? "" : "AND (`Shifts`.`end` < " . time() . " OR `Shifts`.`end` IS NULL)") . "
-      GROUP BY `User`.`UID`
-      ORDER BY `force_active` DESC, `shift_length` DESC" . $limit);
+  $users = User_select_not_tshirt($shift_sum_formula, $show_all_shifts, $limit);
 
   $matched_users = array();
   if ($search == "")
@@ -150,15 +136,15 @@ function admin_active() {
     if ($size != '') {
       $shirt_statistics[] = [
           'size' => $size,
-          'needed' => sql_select_single_cell("SELECT count(*) FROM `User` WHERE `Size`='" . sql_escape($size) . "' AND `Gekommen`=1"),
-          'given' => sql_select_single_cell("SELECT count(*) FROM `User` WHERE `Size`='" . sql_escape($size) . "' AND `Tshirt`=1")
+          'needed' => Shirt_statistics_needed($size),
+          'given' => Shirt_statistics_given($size);
       ];
     }
   }
   $shirt_statistics[] = [
       'size' => '<b>' . _("Sum") . '</b>',
       'needed' => '<b>' . User_arrived_count() . '</b>',
-      'given' => '<b>' . sql_select_single_cell("SELECT count(*) FROM `User` WHERE `Tshirt`=1") . '</b>'
+      'given' => '<b>' . User_tshirts_count() . '</b>'
   ];
 
   return page_with_title(admin_active_title(), array(
